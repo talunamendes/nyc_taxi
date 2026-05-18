d31563f46e3aef99625f6276e7aad028439e92fb
talunamendes <taluna.mendes@gmail.com>
2026-05-18 05:29:29 -0300

    Updating setup files

---
+++ docs/setup/003_create_volumes.sql
@@ -0,0 +1,5 @@
+-- DDL bootstrap script: create volumes
+
+CREATE VOLUME IF NOT EXISTS nyc_taxi_dev.landing.nyc_taxi_raw;
+CREATE VOLUME IF NOT EXISTS nyc_taxi_dev.bronze._checkpoints;
+CREATE VOLUME IF NOT EXISTS nyc_taxi_dev.bronze._schemas;
--- docs/sql/003_create_volumes.sql
+++
@@ -1,5 +0,0 @@
--- DDL bootstrap script: create volumes
-
-CREATE VOLUME IF NOT EXISTS nyc_taxi_dev.landing.nyc_taxi_raw;
-CREATE VOLUME IF NOT EXISTS nyc_taxi_dev.landing._checkpoints;
-CREATE VOLUME IF NOT EXISTS nyc_taxi_dev.landing._schemas;
